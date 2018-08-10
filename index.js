16132839bfd956058bb18a25f26df7f68388fb97
Øyvind Johansen <oyvindj@me.com>
2018-08-10 10:55:26 +0200

    minor changes

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const db = mongoose.connect('mongodb://localhost:27017/users', { useMongoClient: true });
+const db = mongoose.connect('mongodb://localhost:27017/users', {useNewUrlParser: true});
 
 const User = require('./users');
 
 const addUser = (user) => {
    User.create(user).then(user => {
     console.info('new user added...');
-    db.close();
+    mongoose.connection.close();
   });
 }
 
@@ -18,17 +18,17 @@ const findUser = (name) => {
   User.find({$or: [{firstname: search}, {lastname: search}] }).then( user => {
       console.info(user);
       console.info(`${user.length} matches`);
-      db.close();
+      mongoose.connection.close();
     }
   );
 }
 const listUsers = () => {
   User.find({}).then( user => {
-      console.info(user);
+      for(u in user) console.log('firstname: ' + u.firstname);
       console.info(`${user.length} users found`);
-      db.close();
+      mongoose.connection.close();
     }
-  );
+  ).catch(err => console.log('error: ' + err));
 }
 
 module.exports = { addUser, findUser, listUsers }
